69b192e97d4ad30089802d397f358a4abc7eace6
Lazar Nikolov <hello@nikolovlazar.com>
2023-05-06 19:17:21 -0400

    changing project name

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "nuxt-fonts-workshop",
+    title: "vuejs-conf-fonts-workshop",
     htmlAttrs: {
       lang: "en",
     },
