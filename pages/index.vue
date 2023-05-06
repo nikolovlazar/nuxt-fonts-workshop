42b047e85a240e3b54adbf45d78f3813477d182c
Lazar Nikolov <hello@nikolovlazar.com>
2023-05-06 18:59:23 -0400

    initial

---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts"></script>
+
+<template>
+  <ul>
+    <li>
+      <router-link to="/with-google-fonts">With Google Fonts</router-link>
+    </li>
+    <li>
+      <router-link to="/with-modern-font-stacks">
+        With Modern Font Stacks
+      </router-link>
+    </li>
+    <li>
+      <router-link to="/font-display-block">font-display: block</router-link>
+    </li>
+    <li>
+      <router-link to="/font-display-swap">font-display: swap</router-link>
+    </li>
+    <li>
+      <router-link to="/font-display-fallback">
+        font-display: fallback
+      </router-link>
+    </li>
+    <li>
+      <router-link to="/font-display-optional">
+        font-display: optional
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+a {
+  font-size: 1.5rem;
+  line-height: 1.5;
+}
+</style>
---
+++ pages/with-google-fonts.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <h1>Hello, Google Fonts!</h1>
+    <div class="prose">
+      <p>
+        Class suscipit quam sapien ullamcorper viverra sollicitudin eu hendrerit
+        ipsum nisl pretium. Penatibus aptent metus parturient erat nisi vehicula
+        semper aliquam accumsan consequat. Non suscipit cubilia cum urna felis
+        consequat integer, mi aliquet sapien. Praesent nostra duis scelerisque
+        porttitor ipsum tristique curae;? Vulputate habitant turpis mattis purus
+        pretium nostra lorem luctus ultricies porta aliquam porta. Sociis
+        quisque lectus pharetra congue curabitur malesuada. Faucibus sit
+        ultrices bibendum habitant praesent amet. Per lorem tristique ac?
+        Malesuada pulvinar magna turpis aptent condimentum diam! Tortor
+        fermentum libero, in dis mauris facilisi platea. Facilisi sodales
+        potenti parturient. Nibh facilisi erat ut eros sociosqu. Ante facilisis
+        interdum feugiat vitae vitae, donec iaculis lorem nec arcu lorem.
+        Potenti potenti purus blandit aptent.
+      </p>
+      <p>
+        Ante erat morbi ante. Ligula habitasse aliquet platea facilisi pulvinar
+        viverra lacus egestas scelerisque. Cras curabitur elit dictum donec
+        rhoncus mus proin. Mollis nascetur egestas suspendisse consectetur
+        torquent. Mi sed mus; cum nisl curae; phasellus quam porttitor metus
+        massa mattis. Mollis pharetra eros primis sociosqu. Morbi et tincidunt
+        magnis lectus, praesent pulvinar consectetur primis. Tempor hac sociosqu
+        cubilia purus nulla a. Ultricies inceptos fermentum pellentesque
+        himenaeos nisi, cras netus congue rutrum. Lorem nulla ac sit ante neque
+        blandit! Nascetur amet vivamus natoque dolor erat? Tempus proin sit
+        odio, lacinia id. Imperdiet porttitor molestie libero ad sociis.
+        Torquent vitae dictumst malesuada rhoncus pretium est, iaculis velit.
+        Blandit, sollicitudin rhoncus torquent eros suspendisse fermentum
+        lectus.
+      </p>
+      <p>
+        Sit ac vehicula, elit turpis eu congue posuere diam. Viverra orci
+        torquent orci interdum. A netus et hac sodales nostra facilisis. Feugiat
+        torquent condimentum sollicitudin adipiscing. Turpis sociosqu elit vitae
+        et a elementum phasellus etiam urna laoreet sed. Dolor rutrum magna
+        purus nam proin consequat quis rhoncus rhoncus netus est? Lectus semper
+        ultrices aenean proin class. Tincidunt fames sem quis pharetra ad
+        convallis rutrum gravida varius. Sed eu cubilia vivamus ultrices in
+        natoque donec at? Parturient, penatibus aliquam leo. Neque odio iaculis
+        maecenas sagittis eu lacus varius lorem eget pharetra class pharetra.
+        Scelerisque pharetra diam in nullam luctus porta purus. Mattis blandit
+        vivamus ornare eros molestie fringilla semper. Per non dictum massa
+        sociis erat himenaeos convallis ultrices metus id magnis. Adipiscing
+        proin consectetur leo class.
+      </p>
+      <p>
+        Suspendisse ultrices neque purus fusce taciti convallis rutrum himenaeos
+        vulputate dolor. Lobortis laoreet odio nunc himenaeos parturient velit
+        porta lacus blandit vitae. Montes nostra mollis condimentum pulvinar
+        facilisis scelerisque. Rhoncus et euismod urna imperdiet vitae. Eleifend
+        natoque consequat curae; fermentum per. Dignissim, bibendum euismod
+        varius conubia hendrerit vestibulum! Ac lacus laoreet sodales consequat.
+        Consectetur consequat arcu mollis. Lacinia lorem dignissim inceptos,
+        curabitur amet per pharetra proin rutrum class dignissim.
+      </p>
+      <p>
+        Pretium sollicitudin malesuada ante nostra aliquet eleifend sit? Dolor
+        himenaeos, ad tincidunt. Diam fusce dui hac netus iaculis. Facilisis
+        fames torquent quis magnis interdum nam condimentum sed imperdiet a
+        netus! Fames ridiculus id curae; at nascetur amet venenatis. Placerat
+        convallis per turpis iaculis orci feugiat pellentesque consequat
+        vestibulum venenatis. Interdum ligula ac feugiat?
+      </p>
+    </div>
+  </div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@400;700&display=swap");
+h1,
+p {
+  font-family: "IBM Plex Serif", serif;
+}
+</style>
